0fc2fd2837366c126b2f446ccd9c0199a69b5169
test-user <test@email.com>
2020-07-19 21:45:56 +0900

    kryo serializer & registrator

--- pyspark_utils/SparkDriver/GeoSparkDriver.py
+++ pyspark_utils/SparkDriver/GeoSparkDriver.py
@@ -1,7 +1,13 @@
 """GeoSparkDriver"""
 
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
 from geospark.register import upload_jars
 from geospark.register import GeoSparkRegistrator
+from geospark.utils import GeoSparkKryoRegistrator, KryoSerializer
+
+# conf
+from .config import spark_session as conf_spark_session
 
 from .SparkDriver import SparkDriver
 
@@ -15,11 +21,30 @@ class GeoSparkDriver(SparkDriver):
             - not work well in Spark3.0.0
     """
 
-    def __init__(self):
-        self.createSession()
-        spark = self._spark
+    # OVERWRITE SparkSession's method
+    def createSession(self):
+        """
+        create GeoSpark Session
+        """
+        if self.spark is None:
+            appName = conf_spark_session.appName
+            master = conf_spark_session.master
+            conf_list = self.make_conf()
+            spark = SparkSession\
+                .builder\
+                .appName(appName)\
+                .master(master)\
+                .enableHiveSupport()\
+                .config("spark.serializer", KryoSerializer.getName) \
+                .config("spark.kryo.registrator", GeoSparkKryoRegistrator.getName) \
+                .config(conf=SparkConf().setAll(conf_list))\
+                .getOrCreate()
 
-        # for geospark
-        upload_jars()
-        GeoSparkRegistrator.registerAll(spark)
-        self._spark = spark
+            # for GeoSpark
+            upload_jars()
+            GeoSparkRegistrator.registerAll(spark)
+
+            self.spark = spark
+
+        else:
+            logging.warning("WARNING: SparkSession is already created.")
